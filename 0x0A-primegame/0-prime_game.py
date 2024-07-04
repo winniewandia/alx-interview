a3882561d369a300be2615b455502a56de1d6ce1
winniewandia <winniewandia63@gmail.com>
2024-07-04 11:19:08 +0300

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""A function that determines the winner of a prime game"""
+
+
+def isWinner(x, nums):
+    """A function that determines the winner of a prime game
+
+    Args:
+        x (int): number of rounds
+        nums (arr): array of integers
+
+    Returns:
+        str: name of the player who wins the most rounds
+    """
+    if not nums or x < 1:
+        return None
+
+    ben = 0
+    mar = 0
+    for i in range(len(nums)):
+        prime = [True for j in range(i+1)]
+        p = 2
+        while p*p <= i:
+            if prime[p]:
+                for j in range(p*p, i+1, p):
+                    prime[j] = False
+            p += 1
+
+        count = 0
+        for p in range(2, i+1):
+            if prime[p]:
+                count += 1
+            b = 0
+            m = 0
+            if count == 0:
+                b += 1
+                ben += b
+            elif count % 2 == 0:
+                b += 1
+                ben += b
+            else:
+                m += 1
+                mar += m
+    if ben < mar:
+        return "Maria"
+    else:
+        return "Ben"
